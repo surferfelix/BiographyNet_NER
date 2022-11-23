d79f9fba9360f29084d28578156a1227a2f3f397
surferfelix <felixdenheijer@hotmail.com>
2022-11-23 12:16:51 +0100

    Adding counter

--- visualize_stuff.py
+++ visualize_stuff.py
@@ -9,33 +9,58 @@ import matplotlib.pyplot as plt
 import argparse
 
 class Read:
-    '''Can read files from different objects (dir, single_file)'''
+    # TODO Currently only works on json objects
+    '''Can read files from different sources (dir, single_file)'''
     def __init__(self, path: str):
         self.path = path
     
-    def from_directory(self):
+    def from_directory(self) -> dict:
         bio_obj = []
         files = os.listdir(self.path)
         for f in files:
             if not f.startswith('.'):
                 with open(f"{self.path}/{f}", 'r') as json_file:
                     a = json.load(json_file)
-                    return a
+                    bio_obj.append(a)
+        return bio_obj
 
-    def from_file(self):
+    def from_file(self) -> list:
         bio_obj = []
         with open(self.path, 'r') as json_file:
             for line in json_file:
                 bio_obj.append(json.loads(line)) #Appending dict to list
         return bio_obj
 
+class Counter:
+    """Will count instances of key in obj and return a new dictionary object"""
+    # Example Counter(bio_obj, 'named_entities') -> {PER: 113}
+    # TODO: Make this counter object
+    
+    def __init__(self, obj, key):
+        self.obj = obj
+        self.key = key
+    
+    def from_bio_obj(self):
+        '''Takes bio_obj and returns a counter dict'''
+        # This currently just works for the 'text_entities' key
+        counter_obj = dict()
+        # assert type(self.obj[self.key] == list), 'We need to iterate through a list of strings :('
+        for i, lst in enumerate(self.obj):
+            for dct in lst[self.key]:
+                if dct['label'] in counter_obj:
+                    counter_obj[dct['label']] += 1
+                else:
+                    counter_obj[dct['label']] = 1
+        return counter_obj
+        
+    
 class Visualize:
-    def __init__(self, obj: dict, path: str):
-        '''This class can take a dictionary with {str: int}
+    '''This class can take a dictionary with {str: int}
         and visualize it to your liking
         :type obj: dict
         :type path: string
         :path: path to write to'''
+    def __init__(self, obj: dict, path: str):
         self.path = path
         self.obj = obj
     
@@ -56,12 +81,19 @@ class Visualize:
         plt.savefig(self.path)
 
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("path", '-p', type= str, required= True)
-    # parser.parse_args()
     path = '../data/development/json'
     a = Read(path)
-    a.from_directory()
+    bio_obj = a.from_directory()
+    print('This is from a directory')
+    b = Counter(bio_obj, 'text_entities')
+    c = b.from_bio_obj()
+    print(c)
     
+    # path = "/Volumes/Samsung_T5/Text_Mining/MA_Thesis_2/data/development/Allbios_mini.jsonl"
+    # a = Read(path)
+    # b = a.from_file()
+    # Counter(b, 'text_entities')
+    # print('This is from a file')
+    # print(b)
 
 
