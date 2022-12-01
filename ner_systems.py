29a8bf40d670cb8df22a435247dfce56e222a293
surferfelix <felixdenheijer@hotmail.com>
2022-12-01 19:10:10 +0100

    Adding NER system file

---
+++ ner_systems.py
@@ -0,0 +1,31 @@
+"""This file will contain several NER systems that we can test on biographical data"""
+
+from visualize_stuff import Read # We import the read module here so we don't need to rebuild it.
+
+def spacy_model():
+    pass
+
+def flair_model():
+    pass
+
+def stanza_model(): # This model has already been added to the original data, so we don't need to load it here :)
+    pass
+
+def BERT_model(): # This will be the main purpose of our study, finetuning this model for the domain
+    pass
+
+def main():
+    '''Performs experiment'''
+    pass
+
+class Evaluate_Model():
+    '''Takes a model, and evaluates performance'''
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    train = ''
+    dev = ''
+    test = ''
+    validate = ''
+    main()
