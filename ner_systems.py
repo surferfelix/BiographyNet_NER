1923283b14fff68e3498a5b070bbde052231b511
surferfelix <felixdenheijer@hotmail.com>
2022-12-14 13:36:29 +0100

    Adding flair

--- ner_systems.py
+++ ner_systems.py
@@ -1,19 +1,31 @@
-"""This file will contain several NER systems that we can test on biographical data"""
-
-from visualize_stuff import Read # We import the read module here so we don't need to rebuild it.
+"""This file will contain several NER systems that we can test on biographical data,
+we only use PER and LOC labels because all models classify these, which allows us to make proper
+comparisons"""
+import stanza
+from visualize_stuff import Read  # We import the read module here so we don't need to rebuild it.
+from flair.data import Sentence
+from flair.models import SequenceTagger
 # Because sentencepiece in flair does not support python=3.10 yet, we had to change to python version 3.9
 
-class Preprocess_for_Models():
+class Preprocess_for_Models():  
     def __init__(self, bio_obj):
         self.bio_obj = bio_obj
 
     def get_sents_for_train():
         "Will get the sentences for training the model"
         pass
+    
+    def flair_preparation(self):
+        tagger = SequenceTagger.load('flair/ner-dutch')
+        for dct in self.bio_obj:
+            for s in dct["text_sents"]:
+                sentence = ' '.join(s) 
+                flair_piece = Sentence(sentence, use_tokenizer = False)
+                tagger.predict(flair_piece)
+                print(flair_piece)
 
     def stanza_preparation(self):
         ''':return: token, pred, gold'''
-        import stanza
         nlp = stanza.Pipeline(lang = "nl", processors= 'tokenize, ner', tokenize_pretokenized=True)
         gold = [word['label'] for dct in self.bio_obj for word in dct['text_entities']]
         tokens = []
@@ -35,12 +47,7 @@ def spacy_model():
     pass
 
 def flair_model(s):
-    """s: the sentence"""
-    from flair.data import Sentence
-    from flair.models import SequenceTagger
-    sentence = Sentence(s)
-    Tagger = SequenceTagger.load('ner')
-    tagger.predict(sentence)
+    pass
 
     # Flair requires a sentence so we need to preprocess them first
 
@@ -55,10 +62,8 @@ def main(path):
     r = Read(path)
     bio_obj = r.from_tsv() # Currently for dev
     a = Preprocess_for_Models(bio_obj)
-    a.stanza_preparation()
+    a.flair_preparation()
     
-
-
 if __name__ == '__main__':
     train = ''
     dev = '../data/train/AITrainingset1.0/Data/test_NHA.txt'
