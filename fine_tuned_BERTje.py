78bb8d9930a8ba2924e0002e921b1d461caa40b2
surferfelix <felixdenheijer@hotmail.com>
2022-12-21 17:17:11 +0100

    update

--- fine_tuned_BERTje.py
+++ fine_tuned_BERTje.py
@@ -1,4 +1,5 @@
-"""This model is a finetuned version of the BERTje model from wietsedv for named entity recognition on biographical texts"""
+"""This model is a finetuned version of the BERTje model from wietsedv for named entity recognition on biographical texts
+It should be able to detect PER and LOC ner labels"""
 import torch
 from torch.utils.data import Dataset, DataLoader
 from transformers import (AutoConfig, 
@@ -9,7 +10,10 @@ from transformers import (AutoConfig,
                           )
 
 def Fine_Tune_Model():
-    tokenizer = AutoTokenizer.from_pretrained("wietsedv/bert-base-dutch-cased-finetuned-conll2002-ner")
+    # Getting tokenizer
+    tokenizer = AutoTokenizer.from_pretrained("GroNLP/bert-base-dutch-cased")
+    # Loading model 
+    model = AutoModelForSequenceClassification('GroNLP/bert-base-dutch-cased')
     set_seed = 421
     # Number of epochs
     epochs=4
@@ -17,3 +21,24 @@ def Fine_Tune_Model():
     max_length=128
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model_name_or_path = 'wietsedv/bert-base-dutch-cased-finetuned-conll2002-ner'
+
+
+class FineTuned_Biography_Model():
+    def __init__(self, examples):
+        self.n_examples = len(examples)
+    
+    def prepare():
+        """"""
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        model_name = "GroNLP/bert-base-dutch-cased"
+        print('Loading configuration...')
+        model_config = AutoConfig.from_pretrained(pretrained_model_name_or_path=model_name) #TODO n_labels
+        print("Loading tokenizer...")
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        print("Loading model...")
+        # Load model to defined device.
+        model.to(device)
+        print('Model loaded to `%s`'%device)
+        return model
+
+    
