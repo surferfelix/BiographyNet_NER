feabf7d023bac59a2d3283f1a346fc1b5df47f62
surferfelix <felixdenheijer@hotmail.com>
2022-12-21 16:30:27 +0100

    Current version

--- BERTje_model.py
+++ BERTje_model.py
@@ -13,7 +13,7 @@ def run_BERTje(s):
     tokenizer = AutoTokenizer.from_pretrained("wietsedv/bert-base-dutch-cased-finetuned-conll2002-ner")
     model = AutoModelForTokenClassification.from_pretrained("wietsedv/bert-base-dutch-cased-finetuned-conll2002-ner")
     nlp = pipeline("ner", model=model, tokenizer = tokenizer)
-    example = "Ik ben Wolfgang en ik woon in Berlijn"
+    example = s
     ner_results = nlp(example)
     return example, ner_results
 
@@ -24,8 +24,6 @@ def map_tokens_to_entities(text: str, entities):
     
     # Initialize a list of labels with "O" for each token
     labels = ["O" for i in tokens]
-    for i in tokens:
-        print(i)
     
     # Iterate over the entities
     for entity in entities:
@@ -38,12 +36,13 @@ def map_tokens_to_entities(text: str, entities):
         
         # Find the tokens corresponding to the entity
         entity_tokens = text[start:end].split()
-        print(entity_tokens)
         
         # Set the labels for the tokens corresponding to the entity
         for index, token in enumerate(tokens): #TODO Create labels list from here instead
             if token in entity_tokens:
                 labels[index] = label
+    print(tokens)
+    print(labels)
     return tokens, labels
 
 
