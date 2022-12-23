3576a0949c718706011a8617f7971a54b99a50e8
surferfelix <felixdenheijer@hotmail.com>
2022-12-23 10:44:02 +0100

    update

--- fine_tuned_BERTje.py
+++ fine_tuned_BERTje.py
@@ -1,5 +1,6 @@
 """This model is a finetuned version of the BERTje model from wietsedv for named entity recognition on biographical texts
 It should be able to detect PER and LOC ner labels"""
+import tensorflow as tf
 import torch
 from torch.utils.data import Dataset, DataLoader
 from transformers import (AutoConfig, 
@@ -8,27 +9,24 @@ from transformers import (AutoConfig,
                           get_linear_schedule_with_warmup,
                           set_seed,
                           )
+import pandas as pd
 
-def Fine_Tune_Model():
-    # Getting tokenizer
-    tokenizer = AutoTokenizer.from_pretrained("GroNLP/bert-base-dutch-cased")
-    # Loading model 
-    model = AutoModelForSequenceClassification('GroNLP/bert-base-dutch-cased')
-    set_seed = 421
-    # Number of epochs
-    epochs=4
-    batch_size=32
-    max_length=128
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model_name_or_path = 'wietsedv/bert-base-dutch-cased-finetuned-conll2002-ner'
+class BiographyModel(Dataset):
+    def __init__(self):
+        pass
 
 
 class FineTuned_Biography_Model():
     def __init__(self, examples):
         self.n_examples = len(examples)
-    
-    def prepare():
+
+
+    def Read_in_pandas(path):# TODO: For now we read with pandas, but we should implement this into our main read function
+        data = pd.read_csv(path, delimiter = '\t')
+        return data
+    def Load_Models():
         """"""
+        #Check to run from GPU or CPU
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         model_name = "GroNLP/bert-base-dutch-cased"
         print('Loading configuration...')
@@ -36,9 +34,38 @@ class FineTuned_Biography_Model():
         print("Loading tokenizer...")
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         print("Loading model...")
-        # Load model to defined device.
+        model = AutoModelForSequenceClassification(pretrained_model_name_or_path = model_name, config = model_config)
+        # Load model to defined device
         model.to(device)
         print('Model loaded to `%s`'%device)
         return model
 
+    def Load_Dataset():
+        print('Dealing with Train...')
+        # Create pytorch dataset.
+        train_dataset = BiographyModel(path=training_path, 
+                                    use_tokenizer=tokenizer, 
+                                    labels_ids=labels_ids,
+                                    max_sequence_len=max_length)
+
+        print('Created `train_dataset` with %d examples!'%len(train_dataset))
+
+        # Move pytorch dataset into dataloader.
+        train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        print('Created `train_dataloader` with %d batches!'%len(train_dataloader))
+
+        print()
+
+        print('Dealing with ...')
+        # Create pytorch dataset.
+        valid_dataset =  BiographyModel(path=valid_path, 
+                                    use_tokenizer=tokenizer, 
+                                    labels_ids=labels_ids,
+                                    max_sequence_len=max_length)
+
+        print('Created `valid_dataset` with %d examples!'%len(valid_dataset))
+
+        # Move pytorch dataset into dataloader.
+        valid_dataloader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
+        print('Created `eval_dataloader` with %d batches!'%len(valid_dataloader))
     
